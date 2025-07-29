12f03e9af4c35f2d86b602b733bd2cfa0e2dae61
Andres Briñez <brinezlopez08@gmail.com>
2025-07-28 23:04:48 -0500

    Add useAuthLogin hook for authentication handling

---
+++ src/app/core/hooks/useAuth.jsx
@@ -0,0 +1,39 @@
+import { authServiceLogin } from "../service/auth.service";
+import { useContext, useState } from "react";
+import { AppContext } from "../state/AppContext";
+import { useNavigate } from "react-router-dom";
+
+export const useAuthLogin = () => {
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isError, setIsError] = useState(false);
+    const navigate = useNavigate();
+
+    const {dispatch } = useContext(AppContext);
+
+
+
+    const authenticate = (credentials) => authServiceLogin(credentials).then((response) => {
+        const { token, email, userName, roles } = response;
+
+        dispatch({
+            type: "USER_LOGGED",
+            payload: {
+                token: token,
+                email: email,
+                userName: userName,
+                roles: roles,
+            },
+        });
+
+        navigate('/home'); // Redirige a la página de inicio después del login exitoso
+
+
+        setIsError(false);
+    }).catch((error) => {
+        console.error('Error en la autenticación:', error);
+        setIsError(true);
+        setErrorMessage('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+    });
+
+    return { authenticate, errorMessage, isError };
+};
