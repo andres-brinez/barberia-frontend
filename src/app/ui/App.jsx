980d6f1759aa0784d64768d3d7033881dc8ef300
Andres Briñez <brinezlopez08@gmail.com>
2025-08-07 11:28:33 -0500

    refactor: Remove unused imports from App component

--- src/app/ui/App.jsx
+++ src/app/ui/App.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link, RouterProvider } from 'react-router-dom'; // Importa los componentes de React Router
 
 // Importa tus componentes de página
-import LoginPage from './pages/LoginPage/LoginPage';
-import { DashboardLayout } from './layouts/DashboardLayout';
-import Dashboard from './components/Dashboard/Dashboard';
 import { AppProvider } from '../core/state/AppContext';
 import { router } from '../routes/router';
 
